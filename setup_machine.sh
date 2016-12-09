afd8cfe4f954a37481079a8a896a497951362a0a
Amir Alavi <s.amir.alavi@gmail.com>
2016-12-09 03:38:53 -0500

    Set up ssh config through another (private) repo

--- setup_machine.sh
+++ setup_machine.sh
@@ -3,8 +3,12 @@
 # Amir Alavi
 # This script does everything necessary to set up
 # a fresh machine just the way I like it
+REMOTE_REPO_DIR=$HOME/custom_remotes
 echo "Running setup_machine..."
 DIR=$(pwd)
 cd $DIR/scripts/
 /bin/bash install_software.sh
 /bin/bash setup_dotfiles.sh
+git clone https://github.com/AmirAlavi/amirs_remote_machines.git $REMOTE_REPO_DIR
+cd $REMOTE_REPO_DIR
+./configure_ssh.sh
